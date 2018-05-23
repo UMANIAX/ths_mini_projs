e09437564ebbdffe3a18036405c92b34d09147d1
umaniax <umangpahwa@gmail.com>
2018-05-23 16:32:44 +0530

    Nav bar is now sorted

--- clone_hackerearth_for_dev/resp.js
+++ clone_hackerearth_for_dev/resp.js
@@ -1,36 +1,111 @@
-$(document).ready(function(){
+function hide_small() {
 
-    var k = 0;
+    $('#side-ham').hide();
+    $('#sml-sc-nav').hide();
+}
 
-    $("#side-ham").hide();
+function norm_nav (){
 
-    if ($(window).width() < 1120)
-        $("#nrm-sc-nav").hide();
+    hide_small();
+    $('#nrm-sc-nav').show();
+    $('#left-nav').css('margin-left', '-200px');
+    $('#right-nav').css('margin-left', '500px');
+    scroll_toggle(0);
+}
+
+function norm_nav_b1 (){
+
+    hide_small();
+    $('#nrm-sc-nav').show();
+    $('#left-nav').css('margin-left', '-75px');
+    $('#right-nav').css('margin-left', '250px')
+    scroll_toggle(0);
+}
+
+function norm_nav_b2 () {
+
+    hide_small();
+    $('#nrm-sc-nav').show();
+    $('#left-nav').css('margin-left', '-5px')
+    $('#right-nav').css('margin-left', '50px')
+    scroll_toggle(0);
+}
+
+function norm_nav_b3 (){
+
+    hide_small();
+    $('#nrm-sc-nav').show();
+    $('#left-nav').css('margin-left', '-50px')
+    $('#right-nav').css('margin-left', '50px')
+    scroll_toggle(0);
+}
+
+function small_nav (hide_ind){
+
+    if (hide_ind === 1){
+
+        scroll_toggle(hide_ind);
+        $('#side-ham').show();
+    }
+
+    $('#nrm-sc-nav').hide();
+    $('#sml-sc-nav').show();
+}
+
+function scroll_toggle(hide_ind){
+
+    if (hide_ind === 1) {
+
+        $('html').animate({scrollTop: 0}, 1000);
+        $('body').css('overflow', 'hidden');
+    }
 
     else
-        $("#sml-sc-nav").hide();
+        $('body').css('overflow', 'visible');
+}
+
+
+$(document).ready(function(){
+
+    let k = 0;
+
+    if ($(window).width() < 1025)
+        small_nav(k);
+
+    else if ($(window).width() < 1200)
+        norm_nav_b3();
+
+    else if ($(window).width() < 1310)
+        norm_nav_b2();
+
+    else if ($(window).width() < 1500)
+        norm_nav_b1();
+
+    else
+        norm_nav();
 
     $(window).resize(function () {
 
-        if ($(this).width() < 1120) {
-            $("#sml-sc-nav").show();
-            $("#nrm-sc-nav").hide();
+        if ($(this).width() < 1025)
+            small_nav(k);
 
-            if (k === 1)
-                $("#side-ham").show();
-        }
+        else if ($(this).width() < 1200)
+            norm_nav_b3();
 
-        else {
+        else if ($(this).width() < 1310)
+            norm_nav_b2();
 
-            $("#sml-sc-nav").hide();
-            $("#side-ham").hide();
-            $("#nrm-sc-nav").show();
-        }
+        else if ($(this).width() < 1500)
+            norm_nav_b1();
+
+        else
+            norm_nav();
     });
 
     $("#ham-but").click(function () {
 
         $("#side-ham").toggle();
         k = 1 - k;
+        scroll_toggle(k);
     })
 });
