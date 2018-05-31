d0bce54995edb2f4d75c31e58ea9cc55f7f7ac20
umaniax <umangpahwa@gmail.com>
2018-05-31 09:53:41 +0530

    made wiki api work

--- google_image_fetch/map_perform.js
+++ google_image_fetch/map_perform.js
@@ -1,13 +1,26 @@
-$(document).ready(function () {
+$('button').click(function () {
 
-   $('button').click(function () {
+    let street = $('#street').val();
+    let city = $('#city').val();
+    let address = street + ',' + city;
+    let urli = 'https://maps.googleapis.com/maps/api/streetview?size=20000x40000&location=' + address + '&key=AIzaSyBwAwLYKcUFXC7vZNpPv7dOHOhqcwb-76o';
+    // let bg_img = "<img src='"+ urli +"' >";
 
-       let street = $('#street').val();
-       let city = $('#city').val();
-       let address = street + ',' + city;
-       let urli = 'https://maps.googleapis.com/maps/api/streetview?size=3000x3000&location=' + address + '&key=AIzaSyBwAwLYKcUFXC7vZNpPv7dOHOhqcwb-76o';
-       // let bg_img = "<img src='"+ urli +"' >";
+    $('body').css('background-image', "url('" + urli + "')");
 
-       $('body').css('background-image', "url('"+ urli +"')");
-   });
+
+    $.ajax({
+        type: "GET",
+        url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' + address,
+        contentType: "application/jsonp; charset=utf-8",
+        async: false,
+        dataType: "jsonp",
+        success: function (data, textStatus, jqXHR) {
+            document.getElementById('wiki-info').textContent = data[2]
+            // $('div').append("<div style='background-color: white'>'" + data[2] +"'</div>");
+            console.log(data);
+        },
+        error: function (errorMessage) {
+        }
+    });
 });
